df007966af15dfda669f4902347920a7dc7e38db
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-16 14:13:34 +0900

    Update rand_set_arr.c

--- data_structures/rand_set_arr.c
+++ data_structures/rand_set_arr.c
@@ -1,3 +1,6 @@
+// Initialize the elements of array to random numbers of 1 ~ mod
+// the function 'rand()' returns a value in the range of at least 0 ~ (2^15 -1 == 32,767)
+
 void rand_set_arr(int *arr, int size, int mod){
     srand(time(NULL));
     while(size--) arr[size] = rand() %mod +1;
