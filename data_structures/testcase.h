cd8c5025e6a2685e66f98b0629de7c2b9c48ed6b
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-16 14:26:45 +0900

    Update testcase.h

--- data_structures/testcase.h
+++ data_structures/testcase.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define swap(a,b) do{int t = a; a = b, b = t;}while
+#define swap(a,b) do{int t = a; a = b, b = t;}while(0)
 #define print_arr(arr, size) for(int i = 0; i < size; printf("%d ", arr[i++]))
 
 void rand_set_arr(int *arr, int size, int mod){
