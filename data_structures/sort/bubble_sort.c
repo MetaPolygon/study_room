bc7e7848a8bc219471fe5d45aafff5c61d52a715
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-16 14:39:51 +0900

    Create bubble_sort.c

---
+++ data_structures/sort/bubble_sort.c
@@ -0,0 +1,8 @@
+
+//default
+void bubble_sort(int *arr, int n){
+    for (int i = 0; i < n-1; i++)
+        for (int j = n-1; j > i; j--)
+            if (arr[j] < arr[j-1])
+                swap(arr[j], arr[j-1]);
+}
