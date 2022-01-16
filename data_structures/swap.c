91e3b5392ebf0bf7ef5eb0645806caa1a44b7a39
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-16 13:53:16 +0900

    Update swap.c

--- data_structures/swap.c
+++ data_structures/swap.c
@@ -1,7 +1,10 @@
+//stable macro ver
 #define swap(a,b) do{int t = a; a = b, b = t;}while(0)
 
+//macro ver
 #define swap(a,b) {int t = a; a = b, b = t;}
 
+//function ver
 void swap(int a, int b){
   int temp = a;
   a = b, b = temp;
