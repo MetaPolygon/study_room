1812b5f46c2e2146d26499e3d91accc4dfeeb2c3
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-18 18:36:05 +0900

    Update get_prime.c

--- theory/number/get_prime.c
+++ theory/number/get_prime.c
@@ -1,15 +1,17 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
     int n = 1000;
-    int *arr = (int*)calloc(n + 1, sizeof(int));
+    bool *arr = (bool*)calloc(n + 1, sizeof(bool));
     *arr = *(arr + 1) = 1;
 
     for (int i = 2; i * i <= n; i++)
         if (*(arr + i) == 0)
             for (int j = 2 * i; j <= n; j += i)
-                *(arr + j) = 1;
+                *(arr + j) = true;
 
     for (int i = 2; i <= n; i++)
-        if (*(arr + i) == 0) printf("%d ", i);
+        if (*(arr + i) == 0)
+            printf("%d ", i);
 }
