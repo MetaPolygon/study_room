00a3f0d95378713282e556849d4517629936a664
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-18 19:16:20 +0900

    Update get_prime_list.c

--- theory/number/get_prime_list.c
+++ theory/number/get_prime_list.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* get_prime_list(int n, int* idx) {
+//find the prime number between 2 ~ n, and put the number of primes in 'len'
+int* get_prime_list(int n, int* len) {
     bool *nums = (bool*)calloc(n + 1, sizeof(bool));
     *nums = *(nums + 1) = true;
     
@@ -18,17 +19,17 @@ int* get_prime_list(int n, int* idx) {
     int *prime_list = (int*)calloc(cnt, sizeof(int));
     for (int i = 2; i <= n; i++) {
         if (*(nums + i) == false)
-            *(prime_list + (*idx)++) = i;
+            *(prime_list + (*len)++) = i;
     }
     free(nums);
     return prime_list;
 }
 
 int main() {
-    int n = 999, idx = 0;
-    int *prime_list = get_prime_list(n, &idx);
+    int n = 999, len = 0;
+    int *prime_list = get_prime_list(n, &len); // an array of primes between 2 ~ n
     
-    for(int i = 0; i < idx; i++)
+    for(int i = 0; i < len; i++)
         printf("%d ", *(prime_list + i));
     free(prime_list);
 }
