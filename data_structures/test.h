c7443c45eb36198b2b32f3d704af7ccb2382fa66
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-19 18:24:42 +0900

    Update test.h

--- data_structures/test.h
+++ data_structures/test.h
@@ -8,3 +8,15 @@ void rand_set_arr(int *arr, int size, int mod){
     srand(time(NULL));
     while(size--) arr[size] = rand() %mod +1;
 }
+
+// type is '0', it means Ascending.
+// type is '1', it means Descending.
+const char* sort_state(int *arr, int n, int type){
+    int start = arr[0], end = arr[n - 1];
+    for (int i = 0; i < n - 1; i++)
+        if (type == 0 && arr[i] > arr[i + 1] || type == 1 && arr[i] < arr[i + 1])
+            return "Not sorted";
+    if (start == end)
+        return "all same";
+    return type == 0 ? "Ascending" : "descending";
+}
