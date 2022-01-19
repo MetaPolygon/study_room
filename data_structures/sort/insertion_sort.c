fbbfb02eb7e17f55847380fb1d0d219e8871d5e8
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-19 17:58:21 +0900

    Create insertion_sort.c

---
+++ data_structures/sort/insertion_sort.c
@@ -0,0 +1,8 @@
+void insertion_sort(int *arr, int n) {
+    for (int i = 1; i < n; i++) {
+        int temp = arr[i], j;
+        for (j = i; j > 0 && arr[j - 1] > temp; j--)
+            arr[j] = arr[j - 1];
+        arr[j] = temp;
+    }
+}
