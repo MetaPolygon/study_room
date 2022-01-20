a299513f6ee34fcdc186ca6a8c3a6820ab5ac44e
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-20 18:40:45 +0900

    Update sort_state.c

--- data_structures/sort_state.c
+++ data_structures/sort_state.c
@@ -1,11 +1,11 @@
 // type is '0', it means Ascending.
 // type is '1', it means Descending.
-const char* sort_state(int *arr, int n, int type){
+const char* sort_state(int *arr, int n, int order){
     int start = arr[0], end = arr[n - 1];
     for (int i = 0; i < n - 1; i++)
-        if (type == 0 && arr[i] > arr[i + 1] || type == 1 && arr[i] < arr[i + 1])
+        if (order == 0 && arr[i] > arr[i + 1] || order == 1 && arr[i] < arr[i + 1])
             return "Not sorted";
     if (start == end)
         return "all same";
-    return type == 0 ? "Ascending" : "descending";
+    return order == 0 ? "Ascending" : "descending";
 }
