00524b716f3f524a23dbd3cb6b29e73104d75fe2
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-20 18:40:29 +0900

    Update test.h

--- data_structures/test.h
+++ data_structures/test.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #define swap(a,b) do{int t = a; a = b, b = t;}while(0)
+#define create_arr(type, n) (type*)calloc(n, sizeof(type))
 #define print_arr(arr, size) for(int i = 0; i < size; printf("%d ", arr[i++]))
 
 void rand_set_arr(int *arr, int size, int mod){
@@ -11,12 +12,12 @@ void rand_set_arr(int *arr, int size, int mod){
 
 // type is '0', it means Ascending.
 // type is '1', it means Descending.
-const char* sort_state(int *arr, int n, int type){
+const char* sort_state(int *arr, int n, int order){
     int start = arr[0], end = arr[n - 1];
     for (int i = 0; i < n - 1; i++)
-        if (type == 0 && arr[i] > arr[i + 1] || type == 1 && arr[i] < arr[i + 1])
+        if (order == 0 && arr[i] > arr[i + 1] || order == 1 && arr[i] < arr[i + 1])
             return "Not sorted";
     if (start == end)
         return "all same";
-    return type == 0 ? "Ascending" : "descending";
+    return order == 0 ? "Ascending" : "descending";
 }
