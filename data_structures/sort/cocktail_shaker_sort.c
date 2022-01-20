fb4e02870d5cdf52ed6c61ea0906c0ba8332d2e5
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-20 19:43:27 +0900

    Create cocktail_shaker_sort.c

---
+++ data_structures/sort/cocktail_shaker_sort.c
@@ -0,0 +1,21 @@
+void cocktail_shaker_sort(int *arr, int n) {
+    int left = 0, right = n;
+    int shift = left, idx;
+    while (left < right) {
+        idx = left;
+        while (++idx < right)
+            if (arr[idx] < arr[idx - 1]) {
+                swap(arr[idx], arr[idx - 1]);
+                shift = idx;
+            }
+        
+        idx = right = shift;
+        
+        while (left < --idx)
+            if (arr[idx] < arr[idx - 1]) {
+                swap(arr[idx], arr[idx - 1]);
+                shift = idx;
+            }
+        left = shift;
+    }
+}
