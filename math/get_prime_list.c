014c6ef632ab54481d284772af09fcce0e9d74ca
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-20 19:57:39 +0900

    Update get_prime_list.c

--- math/get_prime_list.c
+++ math/get_prime_list.c
@@ -2,23 +2,26 @@
 #include <stdbool.h>
 
 //find the prime number between 2 ~ n, and put the number of primes in 'len'
-int* get_prime_list(int n, int* len) {
-    bool *nums = (bool*)calloc(n + 1, sizeof(bool));
-    *nums = *(nums + 1) = true;
+int* get_prime_list(int* n) {
+    int size = *n;
+    printf("%d::",size);
+    *n = 0;
+    bool *nums = (bool*)calloc(size + 1, sizeof(bool));
+    nums[0] = nums[1] = true;
     
-    int cnt = n - 1;
-    for (int i = 2; i * i <= n; i++)
-        if (*(nums + i) == false)
-            for (int j = i * i; j <= n; j += i)
-                if (*(nums + j) == false){
-                    *(nums + j) = true;
+    int cnt = size - 1;
+    for (int i = 2; i * i <= size; i++)
+        if (nums[i] == false)
+            for (int j = i * i; j <= size; j += i)
+                if (nums[j] == false){
+                    nums[j] = true;
                     cnt--;
                 }
     
     int *prime_list = (int*)calloc(cnt, sizeof(int));
-    for (int i = 2; i <= n; i++)
-        if (*(nums + i) == false)
-            *(prime_list + (*len)++) = i;
+    for (int i = 2; i <= size; i++)
+        if (nums[i] == false)
+            prime_list[(*n)++] = i;
     
     free(nums);
     return prime_list;
