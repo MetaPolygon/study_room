c3afeef08b963fe1c4330769c6eb83daf9c1c072
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-22 10:57:43 +0900

    Update quick_sort.c

--- data_structures/sort/quick_sort.c
+++ data_structures/sort/quick_sort.c
@@ -1,14 +1,14 @@
 //default ver
 void quick_sort(int *arr, int left, int right) {
     int pl = left, pr = right, pivot = arr[(pl + pr) / 2];
-    do {
+    while (pl <= pr) {
         while (arr[pl] < pivot) pl++;
         while (arr[pr] > pivot) pr--;
         if (pl <= pr) {
             swap(arr[pl], arr[pr]);
             pl++, pr--;
         }
-    } while (pl <= pr);
+    }
     if (left < pr) quick_sort(arr, left, pr);
     if (pl < right) quick_sort(arr, pl, right);
 }
