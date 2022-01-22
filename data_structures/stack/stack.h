2c7a427a08339b4b1e94cb4be6e8db72bac48c95
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-22 15:01:19 +0900

    Create stack.h

---
+++ data_structures/stack/stack.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct __stack {
+    int top, max;
+    int *stack;
+}Stack, *pStack;
+
+int initialize(pStack ps, int max);
+int is_full(const pStack ps);
+int is_empty(const pStack ps);
+int push(pStack ps, int val);
+int pop(pStack ps, int *val);
+void print_stack(const pStack ps);
