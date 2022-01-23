8ab8e5b71162f1c7ed88fdc2d99e25ca0e97e0e2
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-23 16:57:29 +0900

    Update test.h

--- data_structures/test.h
+++ data_structures/test.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #define swap(a, b) do{int t = a; a = b, b = t;} while(0)
 #define swap_t(type, a, b) do{type t = a; a = b, b = t;} while(0)
 #define create_arr(n) (int*)calloc(n, sizeof(int))
 #define create_arr_t(type, n) (type*)calloc(n, sizeof(type))
-#define print_arr(arr, size) for(int i = 0; i < size; printf("%2d%c", arr[i++], (i + 1) % 20 ? 32 : 10))
+#define print_arr(arr, size) for(int i = 0; i < size; printf("%2d%c", arr[i++], (i + 1) % 10 ? 32 : 10))
 
 void rand_set_arr(int *arr, int size, int mod){
     srand(time(NULL));
