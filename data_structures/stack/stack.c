52a3a4dd962dce157adb9cd556be6d7ce8a355b7
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-24 18:50:17 +0900

    Update stack.c

--- data_structures/stack/stack.c
+++ data_structures/stack/stack.c
@@ -1,40 +1,40 @@
 #include "stack.h"
 
-int initialize(pStack ps, int max) {
-    ps->top = 0;
-    if ((ps->stack = (int*)calloc(max, sizeof(int))) == NULL) {
-        ps->max = 0;
+int initialize(pStack s, int max) {
+    s->top = 0;
+    if ((s->stack = (int*)calloc(max, sizeof(int))) == NULL) {
+        s->max = 0;
         return -1;
     }
-    ps->max = max;
+    s->max = max;
     return 0;
 }
 
-int is_full(const pStack ps) { return ps->top == ps->max; }
-int is_empty(const pStack ps) { return ps->top == 0; }
+int is_full(const pStack s) { return s->top == s->max; }
+int is_empty(const pStack s) { return s->top == 0; }
 
-int push(pStack ps, int val) {
-    if (is_full(ps))
+int push(pStack s, int val) {
+    if (is_full(s))
         return -1;
-    ps->stack[ps->top++] = val;
+    s->stack[s->top++] = val;
     return 0;
 }
 
-int pop(pStack ps, int *val) {
-    if (is_empty(ps))
+int pop(pStack s, int *val) {
+    if (is_empty(s))
         return -1;
-    *val = ps->stack[ps->top--];
+    *val = s->stack[s->top--];
     return 0;
 }
 
-void print_stack(const pStack ps) {
-    for (int i = 0; i < ps->top; i++)
-        printf("%d ", ps->stack[i]);
+void print_stack(const pStack s) {
+    for (int i = 0; i < s->top; i++)
+        printf("%d ", s->stack[i]);
     putchar(10);
 }
 
 void del_stack(pStack ps) {
-    if (ps->stack != NULL)
-        free(ps->stack);
-    ps->max = ps->top = 0;
+    if (s->stack != NULL)
+        free(s->stack);
+    s->max = s->top = 0;
 }
