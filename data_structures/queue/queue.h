7afeb1c9ad43f4bc78874c60a8457cc4c9f71a3d
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-24 19:09:20 +0900

    Create queue.h

---
+++ data_structures/queue/queue.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct __queue {
+    int max, cnt;
+    int front, rear;
+    int *queue;
+}Queue, *pQueue;
+
+int initialize(pQueue q, int max);
+int is_full(const pQueue q);
+int is_empty(const pQueue q);
+int en_queue(pQueue q, int val);
+int de_queue(pQueue q, int *val);
+void print_queue(const pQueue q);
+void del_stack(pQueue q);
