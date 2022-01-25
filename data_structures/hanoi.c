b63df6a4dfb1dae1d534f6427a3fdb067a48f873
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-25 18:31:29 +0900

    Create hanoi.c

---
+++ data_structures/hanoi.c
@@ -0,0 +1,5 @@
+void hanoi(int n, int from, int to) {
+    if (n > 1) hanoi(n - 1, from, 6 - from - to);
+    printf("%d번째 원반을 %d에서 %d로 이동\n", n, from, to);
+    if (n > 1) hanoi(n - 1, 6 - from - to, to);
+}
