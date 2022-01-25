a16f1e82886cfa2499ca8d87ef381d41d1298f8e
youngrok kim <95115004+MetaPolygon@users.noreply.github.com>
2022-01-25 19:57:02 +0900

    Update queue.h

--- data_structures/queue/queue.h
+++ data_structures/queue/queue.h
@@ -12,5 +12,6 @@ int is_full(const pQueue q);
 int is_empty(const pQueue q);
 int en_queue(pQueue q, int val);
 int de_queue(pQueue q, int *val);
+int search(const pQueue q, int val);
 void print_queue(const pQueue q);
 void del_stack(pQueue q);
