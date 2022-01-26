3247d090a8ca02df2e4ccb1bafcab47e6cbe37ad
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-01-26 19:17:27 +0900

    Update KMP_search.c

--- data_structures/string/string_search/KMP_search.c
+++ data_structures/string/string_search/KMP_search.c
@@ -2,25 +2,25 @@
 
 int KMP_search(const char *str, const char *search) {
     int crsr = 1, cmp_crsr = 0;
-	int skip_table[UCHAR_MAX + 1];
+    int skip_table[UCHAR_MAX + 1];
 	
-	skip_table[crsr] = 0;
-	while (search[crsr] != '\0') {
-		if (search[crsr] == search[cmp_crsr])
-			skip_table[++crsr] = ++cmp_crsr;
-		else if (cmp_crsr == 0) skip_table[++crsr] = cmp_crsr;
-		else cmp_crsr = skip_table[cmp_crsr];
-	}
+    skip_table[crsr] = 0;
+    while (search[crsr] != '\0') {
+        if (search[crsr] == search[cmp_crsr])
+            skip_table[++crsr] = ++cmp_crsr;
+        else if (cmp_crsr == 0) skip_table[++crsr] = cmp_crsr;
+        else cmp_crsr = skip_table[cmp_crsr];
+    }
 
-	crsr = cmp_crsr = 0;
-	while (str[crsr] != '\0' && search[cmp_crsr] != '\0') {
-		if (str[crsr] == search[cmp_crsr])
-			crsr++, cmp_crsr++;
-		else if (cmp_crsr == 0) crsr++;
-		else cmp_crsr = skip_table[cmp_crsr];
-	}
+    crsr = cmp_crsr = 0;
+    while (str[crsr] != '\0' && search[cmp_crsr] != '\0') {
+        if (str[crsr] == search[cmp_crsr])
+            crsr++, cmp_crsr++;
+        else if (cmp_crsr == 0) crsr++;
+        else cmp_crsr = skip_table[cmp_crsr];
+    }
 	
-	if (search[cmp_crsr] == '\0')
-		return crsr - cmp_crsr;
-	return -1;
+    if (search[cmp_crsr] == '\0')
+        return crsr - cmp_crsr;
+    return -1;
 }
