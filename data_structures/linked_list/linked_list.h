9e8b042fd396fe6db5724f665bc961ff1d0f0929
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-01-26 19:43:17 +0900

    Create linked_list.h

---
+++ data_structures/linked_list/linked_list.h
@@ -0,0 +1,19 @@
+#include "Data.h"
+
+typedef struct __node {
+    Data info;
+    struct __node *next;
+}Node, pNode;
+
+typedef struct __list {
+    pNode head;
+    pNode temp;
+}List, pList;
+
+void initialize(pList list);
+pNode search(const pList list, const pNode node);
+void add_node(pList list);
+void print_node(const pList list);
+void print_list(const pList list);
+void del_node(pList list);
+void del_list(pList list);
