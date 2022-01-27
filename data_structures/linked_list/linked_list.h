2a96c744f345f44a334aa1b27ee74cee16da0eda
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-01-27 20:14:19 +0900

    Update linked_list.h

--- data_structures/linked_list/linked_list.h
+++ data_structures/linked_list/linked_list.h
@@ -1,19 +1,22 @@
+#include <stdio.h>
 #include "Data.h"
 
 typedef struct __node {
-    Data info;
+    pData info;
     struct __node *next;
 }Node, *pNode;
 
 typedef struct __list {
+    int cnt;
     pNode head;
-    pNode temp;
+    pNode select;
 }List, *pList;
 
 void initialize(pList list);
 pNode search(const pList list, const pNode node);
-void add_node(pList list);
-void print_node(const pList list);
+void append(pList list, const pData data);
+void insert(pList list, const pData data, int idx);
+void print(const pList list);
 void print_list(const pList list);
-void del_node(pList list);
+void del(pList list);
 void del_list(pList list);
