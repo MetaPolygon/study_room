d02b75b61243545388b5b8c04cf9c65995696366
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-01-29 17:45:33 +0900

    Update Member.c

--- data_structures/linked_list/Member.c
+++ data_structures/linked_list/Member.c
@@ -1,8 +1,39 @@
 #include "Member.h"
 
-void printMember(pMember member)
+static pMember allocMember()
+{
+  return (pMember)calloc(1, sizeof(Member));
+}
+
+static void __scanMember(pMember member, int flag)
+{
+  if (flag == NO) { printf("  no  : "); scanf("%d", &member->no); }
+}
+
+pMember scanMember(int flag)
+{
+  pMember temp = allocMember();
+  if (temp == NULL) {
+    puts(" [allocMember() return 'NULL']");
+  } else {
+    puts(" [input Member info]");
+    if (flag == ALL) {
+      for (int i = ALL; i <= NO; i++)
+        __scanMember(temp, i);
+    } else __scanMember(temp, flag);
+  }
+  return temp;
+}
+
+void printMember(const pMember member)
 {
   printf("\n  --(member)--\n"
            "  |  no : %d  |\n"
            "  ------------\n", member->no);
+}
+
+int cmpMember(const pMember m1, const pMember m2, int flag)
+{
+    if (flag == NO) return m1->no > m2->no ? 1 : 
+                          (m1->no < m2->no ? -1 : 0);
 }
