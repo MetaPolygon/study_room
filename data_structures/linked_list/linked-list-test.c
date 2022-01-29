7a7678c5573da6ba6ef149b30bad2cef465ea32a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-01-29 17:55:02 +0900

    Update and rename main.c to linked-list-test.c

---
+++ data_structures/linked_list/linked-list-test.c
@@ -0,0 +1,15 @@
+
+#include "linked-list.h"
+
+int main()
+{
+  putchar('A');
+  List list;
+    
+  Data data[3] = {{"",1,1.0},{"",2,2.0},{"",3,3.0}};
+    
+  initialize(&list);
+  append(&list, &data[2]);
+  putchar('A');
+  print_list(&list);
+}
