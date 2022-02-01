a0f60bdcb395970723f74dd7cef215118837804d
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-01 18:48:22 +0900

    Create double-circular-linked-list-main.c

---
+++ data_structures/linked_list/double-circular-linked-list-main.c
@@ -0,0 +1,72 @@
+   
+#include "double-circular-linked-list.h"
+
+enum __eMenu
+{
+  EXIT,
+  APPEND,
+  INSERT,
+  SEARCH,
+  PRINT_LIST,
+  DEL_NODE,
+};
+typedef enum __eMenu Menu;
+
+Menu selectMenu(const char *[]);
+
+int main()
+{
+  const char* menu_list[] = {
+    "| Exit                  |",
+    "| Append Node           |",
+    "| Insert Node           |",	
+    "| Search Node           |",
+    "| All Print Node Info   |",
+    "| Delete Node           |",
+  };
+  int menu;
+  DList list;
+  pMember member;
+  initialize(&list);
+  do {
+    switch(menu = selectMenu(menu_list)) {
+      case APPEND:
+      case INSERT: 
+        member = scanMember(ALL);
+        if (menu == APPEND && appendDNode(&list, list.head->prev, member) == -1)
+          puts("  fail append node");
+        else if (menu == INSERT) {
+          if (searchDNode(&list, member) != NULL)
+            member = scanMember(ALL);
+            if (insertDNode(&list, member) == -1)
+              puts("  fail insert node");
+        };
+        break;
+      case SEARCH:
+        if (searchDNode(&list, scanMember(NO)) != NULL)
+          printDNode(&list);
+        break;
+      case PRINT_LIST:
+        printDList(&list);
+        break;
+      case DEL_NODE:
+        if (searchDNode(&list, scanMember(NO)) != NULL)
+          deleteDNode(&list);
+        break;
+      case DEL_LIST: case EXIT:
+        deleteDList(&list);
+        break;
+    }
+  } while (menu != EXIT);
+}
+
+Menu selectMenu(const char *menu[])
+{
+  puts("  -------------Menu------------");
+  for (int i = EXIT; i <= DEL_NODE; i++)    
+    printf(" | (%d) %s\n", i + 1, menu[(i + 1) % (DEL_NODE + 1)]);
+  printf("  -----------------------------\n  select menu : ");
+  int temp;
+  scanf("%d", &temp);
+  return (Menu)(temp % (DEL_NODE + 1));
+}
