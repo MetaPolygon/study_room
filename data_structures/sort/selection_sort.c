0b0a86a7fc128414f2eeded6ff8ab17dc13b628c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-07 18:32:02 +0900

    Update and rename selection_sort.c to selectionSort.c

--- data_structures/sort/selection_sort.c
+++
@@ -1,9 +0,0 @@
-void selection_sort(int *arr, int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-        swap(arr[i], arr[min_idx]);
-    }
-}
