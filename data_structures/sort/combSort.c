0702f378dd1c47d79708e08720945e67f5b37c4c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-07 18:27:09 +0900

    Update and rename comb_sort.c to combSort.c

---
+++ data_structures/sort/combSort.c
@@ -0,0 +1,10 @@
+void combSort(int *arr, int n)
+{
+  double gap = n, shrink_factor = 1.3;
+  while (gap > 1) {
+    gap /= shrink_factor;
+    for (int i = 0; i + gap < n; i++)
+      if (arr[i] > arr[i + (int)gap])
+        swap(arr[i], arr[i + (int)gap]);
+  }
+}
--- data_structures/sort/comb_sort.c
+++
@@ -1,9 +0,0 @@
-void comb_sort(int *arr, int n) {
-    double gap = n, shrink_factor = 1.3;
-    while (gap > 1) {
-        gap /= shrink_factor;
-        for (int i = 0; i + gap < n; i++)
-            if (arr[i] > arr[i + (int)gap])
-                swap(arr[i], arr[i + (int)gap]);
-    }
-}
