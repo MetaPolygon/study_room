1f5af81eeaa55629fe4f6db6f7e6699e63753785
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-07 18:23:19 +0900

    Update and rename bubble_sort.c to bubbleSort.c

---
+++ data_structures/sort/bubbleSort.c
@@ -0,0 +1,22 @@
+//improved ver, set the range to the last exchanged index
+void bubbleSort(int *arr, int n)
+{
+  for (int i = 0; i < n - 1;) {
+    int last_idx = n - 1;
+    for (int j = n - 1; j > i; j--)
+      if (arr[j] < arr[j - 1]) {
+        swap(arr[j], arr[j - 1]);
+        last_idx = j;
+      }
+      i = last_idx;
+  }
+}
+
+//default
+void bubble_sort(int *arr, int n)
+{
+  for (int i = 0; i < n - 1; i++)
+    for (int j = n - 1; j > i; j--)
+      if (arr[j] < arr[j - 1])
+        swap(arr[j], arr[j - 1]);
+}
