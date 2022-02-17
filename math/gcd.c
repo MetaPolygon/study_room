9c8dda49c9424816eb2d7b5b82ac7bfa10e1e3b0
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-17 12:12:45 +0900

    Update gcd.c

--- math/gcd.c
+++ math/gcd.c
@@ -1,6 +1,12 @@
 /*
-
+  gcd(a, b) ≥ 1
+  gcd(a, b) = gcd(|a|, |b|)
+  gcd(|a|, 0) = |a|
+  gcd(a, b) = d,  gcd(a/d, b/d) = 1
+  gdc(a + kb, b) = gcd(a, b)
+  ax + by = gcd(a, b)
 */
+
 int gcd(int a, int b) {
     return b ? gcd(b, a%b) : a;
 }
