0afc0921016c1552f3adcad8311ec3a6ed18975c
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-19 13:54:59 +0900

    Update Euler's phi function.c

--- math/Euler's phi function.c
+++ math/Euler's phi function.c
@@ -1,19 +1,20 @@
 
 /*
   Euler's phi function
-  
-  n이 양의 정수일 때, ϕ(n)은 n과 서로소인 1부터 n이하의 정수의 개수와 같다. ('p' is prime)
+  정수환에 대해 몫환의 가역원을 세는 함수이다.
+  즉, n이 양의 정수일 때 ϕ(n)은 n과 서로소인 1부터 n이하의 정수의 개수와 같다.
+  ϕ(n) = | { i ∈ {1, 2, ... , n} : gdc{n, i} = 1} |
   -----------------------------------------------------
   cnt = 0;
-  for (int i = 1, val; i <= n ; i++)
-    if ((val = gcd(n, i)) == 1)
-      cnt++;
+  for (int i = 1; i <= n ; i++)
+    if (gcd(n, i) == 1) cnt++;
   print("[euelr phi function] retuen val : %d", cnt);
   -----------------------------------------------------  
-  ϕ(n) = n ∏ (1 - 1/p)    {'p' is p ∣ n}
-  ϕ(mn) = ϕ(m)ϕ(n)
-  ϕ(p) = p - 1
-  ϕ(p^k) = p^(k - 1) * (p - 1)
+  (1)   ϕ(n) = n ∏ (1 - 1/p)            Euler Product Foumula {p ∣ n}
+  (2)   ϕ(mn) = ϕ(m)ϕ(n)                {gcd(m, n) = 1}
+  (3-1) ϕ(p) = p - 1
+  (3-2) ϕ(p^k) = p^(k - 1) * (p - 1)
+  
 */
 
 
@@ -22,8 +23,8 @@ int eulerPhiFunc(int n)                               // ϕ(n)
   int euler = n;
   for (int prime = 2; prime * prime <= n; prime++)    // 모든 합성수는 그 수의 제곱근 이하의 약수를 가진다.
     if (n % prime == 0) {
-      euler = euler / p * (p - 1)
+      euler = euler / prime * (prime - 1)
       while (n % prime) n /= prime;
     }
-  return n == 1 ? euler : euler / n * (n - 1)         // euler - 1
-}                                                     // 만약 n이 소수이면, 위의 loop가 적용되지 않으므로, 공식에 따라 처리해준다.
+  return n == 1 ? euler : euler / n * (n - 1)         // 만약 n이 소수이거나, 
+}                                                     
