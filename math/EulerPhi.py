122358075ed41d9e860f7d3ccc215f559ad8423a
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-02-27 18:36:03 +0900

    Update EulerPhi.py

--- math/EulerPhi.py
+++ math/EulerPhi.py
@@ -4,20 +4,18 @@
   정수환에 대해 몫환의 가역원을 세는 함수이다.
   즉, n이 양의 정수일 때 ϕ(n)은 n과 서로소인 1부터 n이하의 정수의 개수와 같다.
   ϕ(n) = | { i ∈ {1, 2, ... , n} : gdc{n, i} = 1} |
-  -----------------------------------------------------
-  cnt = 0;
-  for (int i = 1; i <= n ; i++)
-    if (gcd(n, i) == 1) cnt++;
-  print("[euelr phi function] retuen val : %d", cnt);
-  -----------------------------------------------------  
+  ------------------------------------------------------------------------------
   (1)   ϕ(n) = n ∏ (1 - 1/p)            Euler Product Foumula {p ∣ n}
   (2)   ϕ(mn) = ϕ(m)ϕ(n)                {gcd(m, n) = 1}
   (3-1) ϕ(p) = p - 1
   (3-2) ϕ(p^k) = p^(k - 1) * (p - 1) = p^k - p^(k - 1)
+  (3-3) ϕ(p^k) >= k
   (4)   Σ ϕ(d) = n                      {d | n}
   (5)   Σ dμ(n/d) = ϕ(n)
-  (6)   a^ϕ(n) ≡ 1 (mod n)              {gcd(m, n) = 1}
+  (6-1)   a^ϕ(n) ≡ 1 (mod n)              {gcd(m, n) = 1}
+  (6-2)   a^ϕ(n) ≡ a^2ϕ(n) mod n
   (7)   ϕ(n)이 2의 거듭제곱수일 때, 정 n각형은 눈금없는 자와 컴퍼스로 작도할 수 있다.
+  
   
   [reference]
   https://en.wikipedia.org/wiki/Euler%27s_totient_function
@@ -50,8 +48,13 @@ def EulerPhi(n) :
         prime += 1
     return euler if n == 1 else euler - euler // n
   
-#get EuelrPhi value List
-# ϕ(n) == eulerList[n]
+  
+
+'''
+  (4) Σ ϕ(d) = n,  {d | n}
+  get EuelrPhi value List
+  ϕ(n) == eulerList[n], O(n log n)
+'''
 def EulerPhiList(n) :
     eulerList = [i for i in range(n + 1)]
     for i in range(2, n) :
