c1ea30a187585a68a5e9329b371010ea7071324e
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-03-24 23:18:43 +0900

    Update PollardRho.py

--- math/PollardRho.py
+++ math/PollardRho.py
@@ -1,5 +1,10 @@
 '''
-O(sqrt(P)) <= O(N^¼)
+  Pollard's rho algorithm
+  소인수분해 알고리즘으로 소인수분해하려는 합성수의 가장 작은 소인수의 제곱근데 비례한다.
+  O(sqrt(P)) <= O(N^¼)
+
+  [reference]
+  https://ko.wikipedia.org/wiki/%ED%8F%B4%EB%9D%BC%EB%93%9C_%EB%A1%9C_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
 '''
 from math import gcd
 from random import randint
