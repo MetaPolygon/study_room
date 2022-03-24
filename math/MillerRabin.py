8bbbae13ab5f710cb16fe8abde79a0c7b7f97770
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-03-24 23:17:09 +0900

    Update MillerRabin.py

--- math/MillerRabin.py
+++ math/MillerRabin.py
@@ -1,5 +1,9 @@
 '''
+  Miller-Rabin primality test
+  주어진 수가 소수인지 판별하는 알고리즘이다. 
+  작은 수에 대한 판별은 n < 341,550,071,728,321 일때, a = 2,3,5,7,11,13,17 에 대해서만 검사하면 충분하다. 
   A is count 'a'
+  시간 복잡도는 아래와 같다.
   powmod() : O(N) → O(A log3 N)
   FFT() : O(N) → O(A log2 N)
   [reference]
