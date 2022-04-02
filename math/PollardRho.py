436fac7b16b9f83053ace9d9e4bb58498ceabf4f
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-02 10:36:22 +0900

    Update PollardRho.py

--- math/PollardRho.py
+++ math/PollardRho.py
@@ -9,12 +9,13 @@
 from math import gcd
 from random import randint
 
-def g(x, c, n) :
-    return (x * x + c) % n
-    
+def g(x, c, n) : return (x * x + c) % n
+
 def PollardRho(n) :
     if MillerRabin(n) : return [n]
-    if n == 4 : return [2, 2]
+    if n == 0 : return []
+    if n == 1 : return [1]
+    if n % 2 == 0 : return PollardRho(n // 2) + [2]
     while True :
         c = randint(1, 9)
         x2 = x1 = randint(1, 9)
