4ca846b4a63528d4e5ed90236fe11c57327ba422
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-04-10 09:00:58 +0900

    Update binary_search.c

--- data_structures/binary_search.c
+++ data_structures/binary_search.c
@@ -1,9 +1,8 @@
-int binary_search(int *arr, int n, int key) {
+int binary_search(int *arr, int n, int key)
+{
     int pl = 0, pr = n - 1, pc;
     while (pl <= pr) {
-        if (arr[pc = (pl + pr) / 2] == key)
-            return pc;
-        
+        if (arr[pc = (pl + pr) / 2] == key) return pc;
         if (arr[pc] < key) pl = pc + 1;
         else pr = pc -1;
     }
