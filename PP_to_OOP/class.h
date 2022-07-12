118ce8bbdc9ee01e8000ba2333994e406ecc2b32
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-07-12 23:33:15 +0900

    Update class.h

--- PP_to_OOP/class.h
+++ PP_to_OOP/class.h
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define likeClass(parameter, ...) likeClass_ctor(parameter, __VA_ARGS__)
 //Variadic Macro
 
-typedef struct __likeClass{
-    int num;
-    void (*constructor)();
-    void (*printInfo)();
-}likeClass, *pLikeClass;
+typedef struct _user {
+    struct _user* self;
+    unsigned int age;
+    void (*setAge)();
+    void (*printUserInfo)();
+}User, *pUser;
 
-extern pLikeClass likeClass_ctor(int, ...);
+extern pUser newUser(unsigned int age);
