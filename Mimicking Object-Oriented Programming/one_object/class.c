ee4d5ec8c5c4099408f5a59c29781be667dce4f6
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-09 23:40:28 +0900

    Update class.c

--- Mimicking Object-Oriented Programming/one_object/class.c
+++ Mimicking Object-Oriented Programming/one_object/class.c
@@ -1,42 +1,59 @@
 #include "class.h"
 
-pUser this;
+pUser this; //flag
 
+static void _setName(const char*);
 static void _setAge(uInt);
-static void _setHeight(double);
-static void _setWeight(double);
-static uInt _getAge(void);
-static double _getHeight(void);
-static double _getWeight(void);
-static void _printUserInfo(void);
+static void _setHeight(float);
+static void _setWeight(float);
+static const char* _getName();
+static uInt _getAge();
+static float _getHeight();
+static float _getWeight();
+static void _printUserInfo();
 
-pUser newUser(uInt age, double height, double weight)
+static void initialization()
 {
-    this = (pUser)malloc(sizeof(User));
+    this->setName = _setName;
     this->setAge = _setAge;
     this->setHeight = _setHeight;
     this->setWeight = _setWeight;
+    this->getName = _getName;
     this->getAge = _getAge;
     this->getHeight = _getHeight;
     this->getWeight = _getWeight;
     this->printUserInfo = _printUserInfo;
+}
+static void constructor(const char* name, uInt age, float height, float weight)
+{
+    this->setName(name);
     this->setAge(age);
     this->setHeight(height);
     this->setWeight(weight);
+}
+
+pUser newUser(const char* name, uInt age, float height, float weight)
+{
+    this = (pUser)malloc(sizeof(User));
+    initialization();
+    constructor(name, age, height, weight);
     return this;
 }
 
-static void _setAge   (uInt _age)      { this->age = _age; }
-static void _setHeight(double _height) { this->height = _height; }
-static void _setWeight(double _weight) { this->weight = _weight; }
-static uInt _getAge(void)              { return this->age; }
-static double _getHeight(void)         { return this->height; }
-static double _getWeight(void)         { return this->weight; }
-static void _printUserInfo(void)
+static void _setName(const char* _name) { this->name = _name;}
+static void _setAge(uInt _age)          { this->age = _age; }
+static void _setHeight(float _height)   { this->height = _height; }
+static void _setWeight(float _weight)   { this->weight = _weight; }
+static const char* _getName()           { return this->name; }
+static uInt _getAge()                   { return this->age; }
+static float _getHeight()               { return this->height; }
+static float _getWeight()               { return this->weight; }
+static void _printUserInfo()
 {
     puts(  "-------[user info]-------");
-    printf(" age    : %u\n", this->getAge());
-    printf(" height : %.2lf\n", this->getHeight());
-    printf(" weight : %.2lf\n", this->getWeight());
+    printf("  name   : %s\n", this->getName());
+    printf("  age    : %u\n", this->getAge());
+    printf("  height : %.2f(cm)\n", this->getHeight());
+    printf("  weight : %.2f(kg)\n", this->getWeight());
     puts(  "-------------------------");
 }
