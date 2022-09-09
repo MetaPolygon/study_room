d8015907d8e28af70924ef380ac8436d1e4e33b8
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-09 17:28:34 +0900

    Update class.c

--- Mimicking Object-Oriented Programming/one_object/class.c
+++ Mimicking Object-Oriented Programming/one_object/class.c
@@ -1,32 +1,42 @@
 #include "class.h"
 
-pUser this;//last...
+pUser this;
 
 static void _setAge(uInt);
 static void _setHeight(double);
 static void _setWeight(double);
+static uInt _getAge(void);
+static double _getHeight(void);
+static double _getWeight(void);
 static void _printUserInfo(void);
 
 pUser newUser(uInt age, double height, double weight)
 {
-    this = malloc(sizeof(User));
+    this = (pUser)malloc(sizeof(User));
     this->setAge = _setAge;
     this->setHeight = _setHeight;
     this->setWeight = _setWeight;
-    this->setAge(age);
-    this->setAge(height);
-    this->setAge(weight);
+    this->getAge = _getAge;
+    this->getHeight = _getHeight;
+    this->getWeight = _getWeight;
     this->printUserInfo = _printUserInfo;
+    this->setAge(age);
+    this->setHeight(height);
+    this->setWeight(weight);
     return this;
 }
 
-static void _setAge   (uInt _age)       { this->age = _age; }
+static void _setAge   (uInt _age)      { this->age = _age; }
 static void _setHeight(double _height) { this->height = _height; }
 static void _setWeight(double _weight) { this->weight = _weight; }
-
+static uInt _getAge(void)              { return this->age; }
+static double _getHeight(void)         { return this->height; }
+static double _getWeight(void)         { return this->weight; }
 static void _printUserInfo(void)
 {
-    printf("age    : %u\n", this->age);
-    printf("height : %lf\n", this->height);
-    printf("weight : %lf\n", this->weight);
+    puts(  "-------[user info]-------");
+    printf(" age    : %u\n", this->getAge());
+    printf(" height : %.2lf\n", this->getHeight());
+    printf(" weight : %.2lf\n", this->getWeight());
+    puts(  "-------------------------");
 }
