7343b71ca35770c767a176fb59a94d91b4a5239e
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2022-09-09 17:28:43 +0900

    Update class.h

--- Mimicking Object-Oriented Programming/one_object/class.h
+++ Mimicking Object-Oriented Programming/one_object/class.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef unsigned int uInt;
 typedef struct _user {
     uInt age;
@@ -9,8 +10,10 @@ typedef struct _user {
     void (*setAge)(uInt);
     void (*setHeight)(double);
     void (*setWeight)(double);
+    uInt (*getAge)(void);
+    double (*getHeight)(void);
+    double (*getWeight)(void);
     void (*printUserInfo)(void);
-    
 }User, *pUser;
 
 extern pUser newUser(uInt age, double height, double weight);
